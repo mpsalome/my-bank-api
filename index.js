925033355b244207ff0a230503028dfc51a37c8b
mpsalome <marcus.salome@restoque.com.br>
2020-08-08 19:50:10 -0300

    Created router

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express"
+import accountsRouter from "./routes/accounts.js"
 
 const app = express()
+
 app.use(express.json())
+app.use("/account", accountsRouter)
 
 app.listen(3000, () => {
     console.log("API Started")
